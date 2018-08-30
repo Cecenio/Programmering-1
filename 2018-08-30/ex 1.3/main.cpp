c74ab4cb7a8b650bfca8f70adf4f3d8b5cadc767
cecenio <caesar.brag@gmail.com>
2018-08-30 14:49:48 +0200

    izi save

---
+++ 2018-08-30/ex 1.3/main.cpp
@@ -0,0 +1,16 @@
+//exempel 1.3, sid 18
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	// "-tecknet användsför att 
+	// visa var strängen börjar 
+	// och var den slutar
+	cout << "En textsträng" <<endl;
+	cout << "8======D" <<endl;
+
+	
+	
+	return 0;
+}
