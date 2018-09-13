761c3a51c07b82290b37c05622be1b67dd4eb749
cecenio <caesar.brag@gmail.com>
2018-09-13 13:52:16 +0200

    variabler

---
+++ 2018-09-13/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//variabeldeklaration
+	//vi alokerar (bokar)
+	//plats i datorns minne
+	
+	unsigned int tal;
+	
+	//tal = 2;
+	//cout << tal << endl;
+	//tal få ett värde
+	
+    //cout << "Storlek på short " << sizeof(short) << endl;
+    //cout << "storlek på int " << sizeof(int) << endl;
+	//cout << "storlek på long int " << sizeof(long int) << endl;
+	//se sidan 327
+	//cout << "Storlek på char " << sizeof(char) << endl;
+	
+	char tecken2 = 66; // kan bara deklareras EN gång
+	tecken2 = 65; //Men du kan ge variablen ett nytt värde
+	
+	char tecken = 'B';
+	
+	//cout << tecken << endl;
+	//cout << tecken2 << endl;
+	
+	//Men, du kan deklarera en KONSTANT också
+	
+	const float PI = 3.1415; //Går inte att ändra
+	//Flyttal s variabler (decimaltal)
+	
+	cout << "Storlek på float " << sizeof(float) << endl;
+	cout << "Storlek på double " << sizeof(double) << endl;
+	cout << "Storlek på long double " << sizeof(long double) <<endl; 
+	cout << "Storlek på bool " << sizeof(bool) << endl;
+	
+	int t = 4.96;
+	cout << t << endl; // 4 klipper av deciamldelen
+	int a = 3;
+	int b = 4;
+	int heltalskvot = a/b;
+	cout << "Heltalskvot " << heltalskvot << endl;
+	float flyttalskvot = a/b;
+	cout << "Flyttalskvot  " << flyttalskvot << endl;
+	
+    return 0;	
+}
+ 
